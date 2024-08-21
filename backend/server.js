9cab17a2b0276d370af58f86b8f5db31e3f98037
cameronpool <cameronpool2019@gmail.com>
2024-08-21 11:10:34 -0500

    initial connection to mongodb and some controllers for signup and login/logout created

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,16 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import authRoutes from './routes/auth.routes.js';
+import connectMongoDB from './db/connectMongoDB.js'
+
+dotenv.config();
 
 const app = express()
-const port = 8000;
+const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    res.send('Server is ready')
-});
+app.use('/api/auth', authRoutes);
 
-app.listen(port, () => {
-    console.log(`Server is running on Port: ${port}`)
+app.listen(PORT, () => {
+    console.log(`Server is running on Port: ${PORT}`)
+    connectMongoDB();
 })
